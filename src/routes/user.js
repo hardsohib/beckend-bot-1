576b57d36ffd4150ca189c8797c7508b18e0e923
hardsohib <hardpubgmhokage@gmail.com>
2026-03-04 11:22:28 +0500

    Update user.js

--- src/routes/user.js
+++ src/routes/user.js
@@ -6,11 +6,15 @@ const router = express.Router();
 
 router.get("/me", auth, async (req,res)=>{
   const user = await pool.query(
-    "SELECT id,username,balance,referral_code,is_admin FROM users WHERE id=$1",
+    "SELECT id, username, balance, referral_code, is_admin, receive_notifications FROM users WHERE id=$1",
     [req.user.id]
   );
 
   res.json(user.rows[0]);
 });
+
+  res.json(user.rows[0]);
+});
+
 
 export default router;
