34e582409673f53ee729e84f7f2e38c50f66aa8d
hardsohib <hardpubgmhokage@gmail.com>
2026-03-04 10:28:22 +0500

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
@@ -20,4 +23,5 @@ app.get("/", (req,res)=> res.send("Backend running"));
 
 app.listen(process.env.PORT || 5000, ()=>{
   console.log("Server started");
+
 });
